7094c90672b4b1cf3f9ddc60bb0f70917b1db545
Giovani Nogueira <giovaninogueira2217@gmail.com>
2024-11-22 02:23:51 -0300

    conectando ao banco de dados

--- server.js
+++ server.js
@@ -1,30 +1,42 @@
-    import express from  "express";
+import express from "express";
+import conectarAoBanco from "./src/config/gbConfig.js";
 
-    const posts = [
-      { descrição: "Uma foto", imgurl: "https://placecats.com/millie/300/150", id: 1 },
-      { descrição: "Um gato fofo", imgurl: "https://placekitten.com/200/300", id: 2 },
-      { descrição: "Um gato legal", imgurl: "https://placekitten.com/200/300", id: 3 },
-      
-    ];
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-    const app = express();
-    app.use(express.json());
+const posts = [
+  { descrição: "Uma foto", imgurl: "https://placecats.com/millie/300/150", id: 1 },
+  { descrição: "Um gato fofo", imgurl: "https://placekitten.com/200/300", id: 2 },
+  { descrição: "Um gato legal", imgurl: "https://placekitten.com/200/300", id: 3 },
+];
 
-    app.listen(3000, () => {
-    console.log ("Servidor escutando...");
-    });
-  
-    app.get("/posts", (req, res) => {
-    res.status(200) .json (posts);
-  });
+const app = express();
+app.use(express.json());
 
-  function buscarpostporid(id){
-    return posts.findIndex((post) => {
-      return post.id === Number(id)
-    });
+app.listen(3000, () => {
+  console.log("Servidor aguardando...");
+});
+
+async function getTodosPosts() {
+  const db = conexao.db("imersao-instabytes");
+  const colecao = db.collection("posts");
+  return colecao.find().toArray();
+}
+
+app.get("/posts", async (req, res) => {
+  const posts = await getTodosPosts();
+  res.status(200).json(posts);
+});
+
+function buscarPostPorId(id) {
+  const index = posts.findIndex((post) => post.id === Number(id));
+  if (index !== -1) { 
+    return posts[index];
+  } else {
+    
+    return res.status(404).send("Post não encontrado");
   }
-  app.get("/posts/:id", (req, res) => {
-    const index = buscarpostporid(req.params.id)
-    res.status(200) .json (posts[index]);
-  });
-
+}
+app.get("/posts/:id", (req, res) => {
+  const post = buscarPostPorId(req.params.id);
+  res.status(200).json(post);
+});
